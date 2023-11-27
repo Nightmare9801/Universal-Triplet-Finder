c895989a12f2baed759aff0b2e32c1a798b0deb6
Nightmare9801 <jhumurmondal77@gmail.com>
2023-11-27 13:59:08 +0530

    Updated the comments

--- src/Runner.java
+++ src/Runner.java
@@ -25,13 +25,18 @@ class Triplet{
  */
 class Runner{
     public static void main(String[] args){
+        // Iniatializing the I/O classes
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw= new PrintWriter(System.out, true);
+        // Initializing Triplet class
         Triplet t= new Triplet();
+        // Main Code
         for(;;){
+            // Initialize fundamental parameters
             int k=0;
             ArrayList<ArrayList<Integer>> results= new ArrayList<ArrayList<Integer>>();
             int found=0;
+            // Get the input
             pw.println("Pls enter the depth of the triplets and the search length.");
             String in="";
             try{
@@ -42,10 +47,14 @@ class Runner{
             }catch(IOException e){
                 pw.println(e);
             }
+            //Transform the String to String[] to be used.
             String[] tran= in.split(",");
             try{
+                //Find the Triplets if any
                 for(int i=1;i<=Integer.parseInt(tran[1].replaceAll("\\s","")); i++){
                     boolean rt= true;
+                    // The code is checking if the numbers `i`, `i+1`, and `i+2` form a triplet
+                    // according to a specific mathematical condition.
                     for(int j=2; j<=Integer.parseInt(tran[0].replaceAll("\\s", "")); j++){
                         if(t.isTri(i, i+1, i+2, j)){
                             rt= true;
@@ -54,6 +63,8 @@ class Runner{
                             break;
                         }
                     }
+                    // This code block is executed when a triplet is found that satisfies the given
+                    // mathematical condition.
                     if(rt){
                         results.add(new ArrayList<Integer>());
                         results.get(k).add(i);
@@ -63,6 +74,7 @@ class Runner{
                         found++;
                     }
                 }
+                // Print the results
                 pw.println("Completed...");
                 pw.println("Found: "+found);
                 pw.println(results);
